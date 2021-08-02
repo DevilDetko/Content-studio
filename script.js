564dc48c8c3050fb06473499647355c3f7987c04
Igor <65125429+DevilDetko@users.noreply.github.com>
2021-08-02 11:15:10 +0300

    аыва
    ывавы

---
+++ script.js
@@ -0,0 +1,19 @@
+var secondsInAMinute = 60;
+undefined
+var minutesInAnHour = 60;
+undefined
+var secondsInAnHour = secondsInAMinute * minutesInAnHour;
+secondsInAnHour;
+3600
+var hoursInADay = 24;
+undefined
+var secondsInADay = secondsInAnHour * hoursInADay;
+undefined
+secondsInADay;
+86400
+var daysInAYear = 365;
+undefined
+var secondsInAYear = secondsInADay * daysInAYear;
+undefined
+secondsInAYear;
+31536000
